0ce1722ca78a28d74e29b06350f5a70fd77bc227
Gabriel Queiroz Nunes <gabrielquenu@gmail.com>
2020-08-03 14:13:16 -0300

    feature: landing page built

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import './assets/styles/styles.css';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+};
+
+export default App;
